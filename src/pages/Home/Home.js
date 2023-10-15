b04b465ece8307ca2b5cf98a548c17274825e91c
yavuzim <kyavuzimer@gmail.com>
2023-10-15 03:37:19 +0300

    Home ve Login sayfası tasarlandı.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,27 @@
 import React from 'react'
-
+import './Home.css'
 function Home() {
-  return (
-    <div>
-      home
-    </div>
-  )
+    return (
+        <div className='text-center paper'>
+            <form className='text-center'>
+                <div className='alert alert-primary text-center'>
+                    <h1 className='h1 mb-2 font-weight-normal text-center'>RANDEVU SİSTEMİ</h1>
+                </div>
+                <div className='row'>
+                    <div className='col mb-2 mt-4 pr-1'>
+                        <p className='btn btn-primary'>Randevu Birimi 1</p>
+                    </div>
+                    <div className='col mb-2 mt-4 pr-1'>
+                        <p className='btn btn-primary'>Randevu Birimi 2</p>
+                    </div>
+                    <div className='col mb-2 mt-4 pr-1'>
+                        <p className='btn btn-primary'>Randevu Birimi 3</p>
+                    </div>
+                </div>
+                <p className='mt-5 mb-3 text-muted'>&copy; Sia Teknoloji 2023</p>
+            </form>
+        </div>
+    )
 }
 
 export default Home
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import './Login.css'
+import {FcGoogle} from 'react-icons/fc'
 
 function Login() {
   return (
-    <div>
-      login
+    <div className='text-center'>
+     <form className='form-signin'>
+      <div className='alert alert-secondary'>
+        <h1 className='h3 mb-2 font-weight-normal'>Randevu Alma Sistemi Girişi</h1>
+      </div>
+      <h3 className='mb-3'>Kullanıcı Girişi</h3>
+      <button className='btn btn-dark'>Google İle Giriş Yap</button>
+      <p className='mt-5 mb-3 text-muted'>Randevu Almak İçin <FcGoogle size={'2em'}/>Hesabınız İle Giriş Yapınız</p>
+      <hr/>
+      <h3 className='mb-5'>Yönetici Girişi</h3>
+      <br/>
+      <input type='email' id="email" name="email" className='form-control' placeholder='E-Mail Adresinizi Giriniz...' />
+      <br/>
+      <input type='password' id="password" name="password" className='form-control' placeholder='Şifrenizi Giriniz...' />
+      <br/>
+      <button className='btn btn-outline-primary mr-5'>Yönetici Giriş Yap</button>
+     </form>
     </div>
   )
 }
