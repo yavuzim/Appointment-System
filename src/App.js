5de138c03d13e5afc83b9db29bcb13d259b7a988
yavuzim <kyavuzimer@gmail.com>
2023-10-15 03:16:11 +0300

    router yapisi olusturuldu.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      Randevu Sistemi
+    <div className="body">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
