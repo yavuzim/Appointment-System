eb92981d2da71c88c03462b93847e6f928987641
yavuzim <kyavuzimer@gmail.com>
2023-11-07 11:28:58 +0300

    tarihleri getirmek icin servis olusturuldu.

--- src/features/kullanicilar/kullaniciService.js
+++ src/features/kullanicilar/kullaniciService.js
@@ -1,5 +1,64 @@
-import { auth } from '../../firebase/config'
+import { auth, db } from '../../firebase/config'
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+
+const veriSetleri = [
+    {
+        text: "08:00",
+        deger: 8,
+        pasifMi: false
+    },
+    {
+        text: "09:00",
+        deger: 9,
+        pasifMi: false
+    },
+    {
+        text: "10:00",
+        deger: 10,
+        pasifMi: false
+    },
+    {
+        text: "11:00",
+        deger: 11,
+        pasifMi: false
+    },
+    {
+        text: "12:00",
+        deger: 12,
+        pasifMi: false
+    },
+    {
+        text: "13:00",
+        deger: 13,
+        pasifMi: false
+    },
+    {
+        text: "14:00",
+        deger: 14,
+        pasifMi: false
+    },
+    {
+        text: "15:00",
+        deger: 15,
+        pasifMi: false
+    },
+    {
+        text: "16:00",
+        deger: 16,
+        pasifMi: false
+    },
+    {
+        text: "17:00",
+        deger: 17,
+        pasifMi: false
+    },
+    {
+        text: "18:00",
+        deger: 18,
+        pasifMi: false
+    },
+]
 
 const googleLogin = async () => {
     const provider = new GoogleAuthProvider()
@@ -14,6 +73,35 @@ const googleLogin = async () => {
 
 const kullaniciDoldur = () => {
     return JSON.parse(localStorage.getItem('kullanici'))
+}
+
+const saatleriFormatla = async (veri) => {
+    const pasifTarihlerRef = collection(db, "tarihler")
+    const bugun = new Date()
+    const formatlanmisTarih = bugun.getDate() + "." + (bugun.getMonth() + 1) + "." + bugun.getFullYear()
+
+    let q = query(pasifTarihlerRef, where("tarih", "==", formatlanmisTarih))
+    q = query(q, where("birim", "==", veri))
+
+    const snaps = await getDocs(q)
+    let veriSetler = veriSetleri
+
+    if (!snaps.empty) {
+        snaps.forEach(doc => {
+            doc.data().saatler.map(saat => {
+                veriSetler = veriSetler.filter(function (veri) {
+                    if (veri.deger !== saat.deger) {
+                        return veri
+                    }
+                })
+                veriSetler.push(saat)
+            })
+        })
+        veriSetler.sort(function (a, b) {
+            return a.deger - b.deger
+        })
+    }
+    return veriSetler
 }
 
 const kullaniciService = {
